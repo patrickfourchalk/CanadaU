cdb91601ea514da79ce9d6e936ce8c107165f535
Patrick Fourchalk <48849844+pgzfourc@users.noreply.github.com>
2019-11-26 22:57:12 -0500

    Update app.py

--- python/app.py
+++ python/app.py
@@ -8,7 +8,7 @@ import os
 
 import praw
 import prawcore
-from flask import Flask
+from flask import Flask, send_from_directory
 
 # Create our Reddit client to communicate with Reddit
 reddit = praw.Reddit(client_id='wJhXch1ro-Dn0A',
@@ -16,7 +16,15 @@ reddit = praw.Reddit(client_id='wJhXch1ro-Dn0A',
                      user_agent='web:CanadaU:1.0 (by /u/rufhvbp)')
 
 # Create our Flask application
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../react-app/build')
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def serve(path):
+    if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
 
 # Add an endpoint to our Flask application
 @app.route('/subreddit/<subreddit_id>')
@@ -49,6 +57,20 @@ def banner(subreddit_id):
 		return ("Subreddit not found", 404)
 	except prawcore.exceptions.NotFound:
 		return ("Subreddit not found", 404)
+		
+subredditIDs = ["uwaterloo", "UBC", "Uoft"]
+		
+@app.route('/subreddits')
+def subreddits():
+	subreddits = {};
+	for subredditID in subredditIDs:
+		subs = reddit.subreddit(subredditID)
+		subreddits[subredditID] = {
+			"display_name": subs.display_name,
+			"subscribers": subs.subscribers,
+			"icon": subs.icon_img
+		}
+	return (subreddits, 200)
 	
 if __name__ == "__main__":
     app.run()
